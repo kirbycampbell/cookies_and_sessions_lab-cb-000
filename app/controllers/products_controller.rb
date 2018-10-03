57874c360ec4d404859a4beee6b90cecfc13ea50
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-03 00:13:02 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,14 @@ class ProductsController < ApplicationController
   end
 
   def add
+    @product = Product.new(product_params)
+  end
 
+
+  private
+
+  def product_params
+    params.require(:products).permit(:name)
   end
 
 end
