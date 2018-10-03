415a42cf7636f2637ee58256b2faa53fb31dc8b8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-03 00:16:33 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
+
       redirect_to @product
     else
       render :add alert: "Add Item PLZ"
