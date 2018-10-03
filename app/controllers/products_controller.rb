94ad98c11b7371cda492c86b895438ea5afd9bf2
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-03 00:20:49 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all
   end
 
   def add
-    @product = Product.new(product_params)
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-
-      redirect_to @product
-    else
-      render :add
-    end
-  end
-
-
-  private
-
-  def product_params
-    params.require(:products).permit(:name)
+    cart << params[:product]
+    render :index
   end
 
 end
